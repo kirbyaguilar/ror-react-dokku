27fefcc3fc0df129b4367442a536c0971448bfc0
kirbyaguilar <samuelkirbyaguilar@gmail.com>
2024-10-08 13:39:04 +0800

    feat: install axios + add fetching to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
-import { useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 export default function App() {
-  const [text, setText] = useState('aaaa');
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/api/employees');
+        const employees = response.data;
+
+        if (employees) {
+          setText(employees.data);
+        }
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);  // empty dependency array since the effect should run once on mount
 
   return (
     <div>
